ffe87960582d02fa772fdbc9db6acda3c05b2b59
joshhewlett <jhewlett@asu.edu>
2016-09-26 15:18:36 -0700

    If-else.. Order is critical

--- DecisionApp/www/js/result.js
+++ DecisionApp/www/js/result.js
@@ -4,7 +4,10 @@ window.onload = function() {
     
     //Changes Maps link to appropriate OS specifications
 	result = parent.document.URL.substring(parent.document.URL.indexOf('?') + 8, parent.document.URL.length);
+    console.log(result);
 	document.getElementById("result").innerHTML = result.replace(/%20/g, " ");
+    
+    
 	deviceType = (navigator.userAgent.match(/iPhone/i))  == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : "null";
 	
 
@@ -12,7 +15,7 @@ window.onload = function() {
 		var mapsLink = "maps:q=" + result;
 		document.getElementById("mapsLink").href = mapsLink;
 	}
-	if else (deviceType === "Android") {
+	else if (deviceType === "Android") {
 		var mapsLink = "geo:0,0?q=" + result;
 		document.getElementById("mapsLink").href = mapsLink;
         
